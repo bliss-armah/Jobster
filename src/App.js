9d3b3c36e0ed3a0196fb912211f5512e63434bf7
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-13 09:12:01 +0000

    styled component setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import Landing from "./pages/Landing";
+import styled from 'styled-components'
+
+
+const Button = styled.button`
+background:red;
+color: white;
+font-size: 2rem;
+
+`
+
+const SecondButton = styled.button`
+background:blue;
+color: yellow;
+font-size: 3rem;
+
+`
 
 function App() {
   return (
    <div>
+    <Button>click me</Button>
+    <SecondButton>click me</SecondButton>
     <Landing/>
    </div>
   );
