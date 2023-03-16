32e6e4fe68af3c8981cb440a92aeb97007483576
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-16 16:06:04 +0000

    Authentication reducer

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { Logo, FormRow } from "../components";
 import { toast } from "react-toastify";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useSelector, useDispatch } from "react-redux";
+import { loginUser, registerUser } from "../features/userSlice";
 
 const initialState = {
   name: "",
@@ -13,51 +15,57 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
-    const name = e.target.name
-    const value = e.target.value
-    setValues({...values, [name]:value})
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const {name,email,password,isMember} = values
-    if (!email || !password || (!isMember && !name )) {
-      toast.error('Please fill out all field');
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error("Please fill out all field");
+      return;
     }
-    
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }));
+      return;
+    } 
+    dispatch(registerUser({ name, email, password }));
   };
 
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
 
-  const toggleMember = () =>{
-    setValues({...values, isMember : !values.isMember})
-  }
-
-  return ( 
+  return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>{values.isMember?"Login":"Register"}</h3>
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
         {/* name field */}
-        {!values.isMember &&
-        <FormRow
-          type='text'
-          name="name"
-          value={values.name}
-          handleChange={handleChange}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
           />
-        }
+        )}
         {/* email field */}
         <FormRow
-        type='email'
-        name="email"
-        value={values.email}
-        handleChange={handleChange}
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
         />
         {/* password field */}
         <FormRow
-          type='password'
+          type="password"
           name="password"
           value={values.password}
           handleChange={handleChange}
@@ -66,8 +74,10 @@ const Register = () => {
           submit
         </button>
         <p>
-          {values.isMember?'Not a member yet?':'Already a member?'}
-          <button type="button" onClick={toggleMember} className="member-btn">{values.isMember?'Register':'Login'}</button>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
         </p>
       </form>
     </Wrapper>
