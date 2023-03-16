44925a98f111b2a45ba336159425beaaa675f547
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-16 15:37:58 +0000

    userslice setup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,41 +1,77 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { Logo } from '../components';
-import Wrapper from '../assets/wrappers/RegisterPage';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import { toast } from "react-toastify";
+import Wrapper from "../assets/wrappers/RegisterPage";
 
 const initialState = {
-  name:'',
-  email:'',
-  password:'',
-  isMember:true,
-}
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  const [values,setValues] = useState(initialState)
+  const [values, setValues] = useState(initialState);
 
-  const handleChange = (e) =>{
-    console.log(e.target);
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setValues({...values, [name]:value})
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const {name,email,password,isMember} = values
+    if (!email || !password || (!isMember && !name )) {
+      toast.error('Please fill out all field');
+    }
+    
+  };
+
+
+  const toggleMember = () =>{
+    setValues({...values, isMember : !values.isMember})
   }
 
-  const onSubmit = (e) =>{
-    e.preventDefault()
-    console.log(e.target);
-  }
-  return (
-    <Wrapper className='full-page'>
-      <form className='form' onSubmit={onSubmit}>
+  return ( 
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>login</h3>
+        <h3>{values.isMember?"Login":"Register"}</h3>
         {/* name field */}
-        <div className="form-row">
-          <label htmlFor="name" className='form-label'>name</label>
-          <input type="text" name='name' value={values.name} onChange={handleChange} 
-          className='form-input' />
-        </div>
-        <button type='submit' className='btn btn-block'>submit</button>
+        {!values.isMember &&
+        <FormRow
+          type='text'
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+          />
+        }
+        {/* email field */}
+        <FormRow
+        type='email'
+        name="email"
+        value={values.email}
+        handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type='password'
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          {values.isMember?'Not a member yet?':'Already a member?'}
+          <button type="button" onClick={toggleMember} className="member-btn">{values.isMember?'Register':'Login'}</button>
+        </p>
       </form>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
