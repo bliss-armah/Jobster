aefa64abd2ecf5dbcaae816727bfa77521750ef9
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-20 14:02:32 +0000

    update user profile

--- src/features/user/userThunk.js
+++ src/features/user/userThunk.js
@@ -1,11 +1,34 @@
 import { logoutUser } from "../userSlice";
-import customFetch from "../utils/axios";
+import customFetch from "../../utils/axios";
 
-export const registerUserThunk = async(url,user,thunkAPI)=>{
-    try {
-        const resp = await customFetch.post("auth/register", user);
-        return resp.data;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error.response.data.msg);
-      }
-}
+export const registerUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.post(url, user);
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
+
+export const loginUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.post(url, user);
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
+
+export const updateUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.patch(url, user, {
+      headers: {
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    });
+    return resp.data;
+  } catch (error) {
+    console.log(error.respons);
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
