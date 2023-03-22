f57c40df4231f5b6c7750d1004c885b09f9f7d2a
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-22 11:33:41 +0000

    edit job logic withh http request done

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -2,9 +2,13 @@ import { FormRow, FormRowSelect } from "../../components";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-import { handleChange,clearValues,createJob } from "../../features/job/jobSlice";
+import {
+  handleChange,
+  clearValues,
+  createJob,
+  editJob,
+} from "../../features/job/jobSlice";
 import { useEffect } from "react";
-
 
 const AddJob = () => {
   const {
@@ -19,33 +23,45 @@ const AddJob = () => {
     isEditing,
     editJobId,
   } = useSelector((store) => store.job);
-  const {user} = useSelector((store)=>store.user)
-  const dispatch = useDispatch()
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (!position || !company || !jobLocation) {
       toast.error("Please fill out all fields");
+      return;
+    }
+    if (isEditing) {
+      dispatch(
+        editJob({
+          jobId: editJobId,
+          job: { position, company, jobLocation, jobType, status },
+        })
+      );
       return
     }
-    dispatch(createJob({position,company,jobLocation,jobType,status}))
+    dispatch(createJob({ position, company, jobLocation, jobType, status }));
   };
 
   const handleJobInput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    dispatch(handleChange({name,value}))
+    dispatch(handleChange({ name, value }));
     console.log(value);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     if (!isEditing) {
-      dispatch(handleChange({
-        name:'jobLocation',value:user.location,
-      }))
+      dispatch(
+        handleChange({
+          name: "jobLocation",
+          value: user.location,
+        })
+      );
     }
-  },[])
+  }, []);
 
   return (
     <Wrapper>
@@ -93,7 +109,7 @@ const AddJob = () => {
             <button
               type="button"
               className="btn btn-block clear-btn"
-              onClick={()=>dispatch(clearValues())}
+              onClick={() => dispatch(clearValues())}
             >
               clear
             </button>
