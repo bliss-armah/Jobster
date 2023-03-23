e871a5c34fded3c553b221537c8ac29fe2b329eb
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-23 13:08:25 +0000

    debounce funtionality on search params

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -3,21 +3,41 @@ import { FormRow, FormRowSelect } from ".";
 import Wrapper from "../assets/wrappers/SearchContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { handleChange,clearFilters } from "../features/allJobs/allJobsSlice";
+import { useState, useMemo } from 'react';
 
 const SearchContainer = () => {
+  const [localSearch,setLocalSearch] = useState('')
+
   const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
     useSelector((store) => store.allJobs);
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
   const dispatch = useDispatch();
 
   const handleSearch = (e) => {
-    if (isLoading) return
+    
     dispatch(handleChange({ name: e.target.name, value: e.target.value }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLocalSearch('')
     dispatch(clearFilters())
   };
+
+
+  const debounce = () =>{
+    console.log('debounce called');
+    let timeoutID
+    return (e) =>{
+      setLocalSearch(e.target.value)
+      clearTimeout(timeoutID)
+      timeoutID = setTimeout(() => {
+    dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+      }, 1500);
+    }
+  }
+
+  const optimizedDebounce = useMemo(()=>debounce(),[])
+
   return (
     <Wrapper>
       <form className="form">
@@ -27,15 +47,15 @@ const SearchContainer = () => {
           <FormRow
             type="text"
             name="search"
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
           {/* search by status */}
           <FormRowSelect
             labelText="status"
             name="searchStatus"
             value={searchStatus}
-            handleChange={handleSearch}
+            handleChange={handleChange}
             list={["all", ...statusOptions]}
           />
           {/* search by type */}
