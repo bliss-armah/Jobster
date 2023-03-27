3e1e5ddb805a33a10ec1f4a859a66593028bd95b
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-03-27 08:01:07 +0000

    outside click to close modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,68 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/Navbar';
-import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
-import Logo from './Logo';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleSidebar,clearStore } from '../features/userSlice';
+import React from "react";
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import Logo from "./Logo";
+import { useState, useEffect,useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleSidebar, clearStore } from "../features/userSlice";
 
 const Navbar = () => {
-  const [showLogout,setShowLogout] = useState(false)
-  const {user} =useSelector((store)=>store.user)
-  const dispatch =useDispatch()
+  const [showLogout, setShowLogout] = useState(false);
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
 
-  const toggle = () =>{
-    dispatch(toggleSidebar())
-  }
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
 
+  let menuRef = useRef()
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setShowLogout(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () =>{
+      document.removeEventListener("mousedown", handler)
+    }
+  });
 
   return (
     <Wrapper>
-    <div className="nav-center">
-      <button type='button' className='toggle-btn' onClick={toggle}><FaAlignLeft/></button>
-    <div>
-
-    <Logo/>
-    <h3 className='logo-text'>dashboard</h3>
-    </div>
-    <div className="btn-container">
-      <button type='button' className='btn' onClick={()=>setShowLogout(!showLogout)}>
-        <FaUserCircle/>{user?.name}<FaCaretDown/>
-      </button>
-    <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'} >
-      <button type='button' className='dropdown-btn' onClick={()=>dispatch(clearStore('Logging out...'))}>
-        logout
-      </button>
-    </div>
-    </div>
-    </div>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggle}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container" ref={menuRef}>
+          <button
+            type="button"
+            className="btn"
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => dispatch(clearStore("Logging out..."))}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
